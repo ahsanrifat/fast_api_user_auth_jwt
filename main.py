170732be4efffe8ad94bad4ab3ca4ef684a09afb
Syed Rifat Ahsan <frifat2013@gmail.com>
2022-03-17 12:19:55 +0600

    Added post method, ran the dunction on another port

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
+
+
+class user(BaseModel):
+    name: str
+    email: str
+    password: str
+    is_admin: Optional[bool] = False
+    is_super_admin: Optional[bool] = False
+
 
 app = FastAPI()
 
 
+# operation get
+# path operation decorator
 @app.get("/")
+# path operation function
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/user/{id}")
+def get_a_single_user(id: int, qry: Optional[str] = None):
+    return {"user": id, "param": qry}
+
+
+@app.post("/user/create/")
+def create_user(user: user):
+    return user
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=9000)
