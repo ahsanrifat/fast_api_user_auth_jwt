e0a1d43a4f25cbe0c374a7cbf0308f3f50d9edf0
Syed Rifat Ahsan <frifat2013@gmail.com>
2022-03-19 16:58:27 +0600

    Added delete, put apis for user

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Depends
+from urllib import response
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from typing import Optional
 import uvicorn
 import schemas
@@ -37,15 +38,48 @@ def get_all_users_list(db: Session = Depends(get_db)):
     return {"data": users}
 
 
+@app.delete("/user/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_an_user(id, db: Session = Depends(get_db)):
+    db.query(models.User).filter(models.User.id == id).delete(synchronize_session=False)
+    db.commit()
+    return {"status": True, "message": f"User {id} deleted successfully"}
+
+
 @app.get("/user/{id}")
 def get_a_single_user(
-    id: int, qry: Optional[str] = None, db: Session = Depends(get_db)
+    id: int,
+    response: Response,
+    qry: Optional[str] = None,
+    db: Session = Depends(get_db),
 ):
     user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"status": False, "message": "No user found"}
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={"status": False, "message": f"No user found with id {id}"},
+        )
     return {"data": user}
 
 
-@app.post("/user/create/")
+@app.put("/user/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update_an_user(id, request_body: schemas.User, db: Session = Depends(get_db)):
+    db.query(models.User).filter(models.User.id == id).update(
+        {
+            "name": request_body.name,
+            "password": request_body.password,
+            "email": request_body.email,
+            "is_active": request_body.is_active,
+            "type": request_body.type,
+        },
+        synchronize_session=False,
+    )
+    db.commit()
+    return {"status": True, "message": f"User {id} updated successfully"}
+
+
+@app.post("/user/create/", status_code=status.HTTP_201_CREATED)
 def create_user(user: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(
         name=user.name,
